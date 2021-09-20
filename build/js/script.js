798ba6e45c6c8d5546b79b2c9008903be0fd678c
unifiedigital-studios <unifiedigitalstudios@outlook.com>
2021-09-20 07:16:34 +0100

    How to use var, let, const
    Examples uses for var, let, const

--- build/js/script.js
+++ build/js/script.js
@@ -1,2 +1,19 @@
 // TEST COMMAND
-console.log("Hello World!")
+console.log("Hello World!");
+
+// Variables & Data Types
+
+/* 
+var[Rarely Used], let, const
+NOTE: Use "const" Unless Reassigning The Value Is NEEDED (Read-Only)
+Data Types: String, Number, BigInt, Booleon, null, undefined, Symbols 
+*/
+
+// Example of How to use [BEFORE ES6] var => function-scoped, [AFTER ES6 (ES2015)] let, const => block-scoped
+var todayDateObject = new Date();
+let [username, password] = ['test', 1234];
+const project = "Open Source";
+
+console.log(todayDateObject);
+console.log(username, password);
+console.log(project);
